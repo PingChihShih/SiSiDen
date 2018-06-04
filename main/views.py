d34aef989bb9ab29ce9f6c584b565b6198dd7154
pingchihshih <b03902039@ntu.edu.tw>
2018-06-04 18:12:05 +0800

    index.html almost, plan.html started

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'index.html')
+
+def plan(request):
+	return render(request, 'plan.html')
+
+def attend(request):
+	return render(request, 'attend.html')
