2cd2b7d0e7657902b806a5112e01c0ce42d669bd
pingchihshih <b03902039@ntu.edu.tw>
2018-06-06 16:59:00 +0800

    change project name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SiSiDen.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WifiOrderingSystem.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
