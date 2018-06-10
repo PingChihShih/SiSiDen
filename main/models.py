9c989b50b486e86a4ea58a863444c4cdf05c136e
pingchihshih <b03902039@ntu.edu.tw>
2018-06-10 11:48:20 +0800

    modify templates with django tags

--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class Item(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=100)
+
+class Customer(models.Model):
+    table_id = models.DecimalField(max_digits=2, decimal_places=0)
+    password = models.CharField(max_length=30)
+
+class Order(models.Model):
+    order_id = models.DecimalField(max_digits=5, decimal_places=0)
+
+class SingleOrder(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    temp_choices = (
+        ("iced", "正常冰"),
+        ("little-iced", "少冰"),
+        ("cool", "去冰"),
+        ("hot", "熱"),
+    )
+    temp = models.CharField(max_length=4, choices=temp_choices)
+    sugar_choices = (
+        ("normal-sugar", "正常糖"),
+        ("little-sugar", "少糖"),
+        ("no-sugar", "無糖"),
+    )
+    sugar = models.CharField(max_length=4, choices=sugar_choices)
+    count = models.DecimalField(max_digits=2, decimal_places=0)
+    # comment = models.CharField(max_length=100, blank=True)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from .models import Item, SingleOrder
+from .forms import SingleOrderForm
 
 # Create your views here.
+
 def order(request):
-	return render(request, 'order.html')
+	item = Item()
+	single_order = SingleOrder()
+	item.name = "豪郝喝咖啡"
+	item.description = "豪郝喝~~~"
+	form = SingleOrderForm()
+
+	if request.method == 'POST':
+		form = SingleOrderForm(request.POST)
+		if form.is_valid():
+			single_order.name = item.name
+			single_order.temp = form.cleaned_data['single']
+
+
+	arg = {'item': item, 'form': form}
+	return render(request, 'order.html', arg)
 
 def result(request):
 	return render(request, 'result.html')
