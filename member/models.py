7f9a7a505ad37ab750c5563012ebe9b925749a8d
Jonah246 <b03902085@ntu.edu.tw>
2018-06-13 15:50:35 +0800

    ssl and model

--- member/models.py
+++ member/models.py
@@ -3,12 +3,7 @@ from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
                                         PermissionsMixin)
 
-
-class Member(AbstractBaseUser, PermissionsMixin):
-    """
-    total_point: This is the point which is consumed for growing a tree.
-    app_point: This is the basic point of this app.
-    """
+class Member(AbstractBaseUser):
     table_id = models.IntegerField()
 
 
